2e59dbde3ffee4f4dc58aeeddfa2a69cfccc8e2b
gaue2708 <etienne.gauvin-clermont@usherbrooke.ca>
2021-11-23 23:56:20 -0500

    Add Input network packet

--- include/pac_network.h
+++ include/pac_network.h
@@ -4,15 +4,11 @@ const size_t MAX_DATAFRAME_LEN = 1024; // 1518 default max linux;
 
 enum NETWORK_PACKET_TYPE
 {
-    START_FRAME,
-    END_FRAME,
-    VIDEO,
-    FLUSH_SCREEN
+    INPUT
 };
 
 typedef struct _NetworkPacket
 {
     NETWORK_PACKET_TYPE packet_type;
-    int buffer_offset;
-    char data[MAX_DATAFRAME_LEN - sizeof(packet_type) - sizeof(buffer_offset)];
+    char data[MAX_DATAFRAME_LEN - sizeof(packet_type)];
 } NetworkPacket;
